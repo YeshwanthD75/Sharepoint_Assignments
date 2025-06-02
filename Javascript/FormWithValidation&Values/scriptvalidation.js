2b50f437803bb2515a66aca363d6f0aac1763eb5
Yeshwanth D <166380375+YeshwanthD75@users.noreply.github.com>
2025-06-02 17:10:36 +0530

    Add files via upload

---
+++ Javascript/FormWithValidation&Values/scriptvalidation.js
@@ -0,0 +1,17 @@
+function changeFormat(n)
+
+{
+
+if (n==1){
+
+document.getElementById("style").href = "Format1.css";
+
+}else if (n==2){
+
+document.getElementById("style").href = "Format2.css";
+
+}else if (n==3)
+
+document.getElementById("style").href = "Format3.css";
+
+}
